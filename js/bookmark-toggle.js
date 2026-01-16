aed6f40fad9f9ccba51e4587c32636f0c65e3358
y-jh107 <jin99545@naver.com>
2026-01-16 13:46:40 +0900

    북마크바 만들기

---
+++ js/bookmark-toggle.js
@@ -0,0 +1,40 @@
+(function () {
+  const bookmarkBar = document.getElementById("bookmark-bar");
+  const bookmarkOpen = document.getElementById("bookmark-open");
+  const bookmarkClose = document.getElementById("bookmark-close");
+
+  const isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
+  if (isBookMarkBarOpen === "close") {
+    localStorage.setItem("isBookMarkBarOpen", "close");
+    bookmarkBar.style.display = "none";
+    bookmarkOpen.style.display = "none";
+    bookmarkClose.style.display = "flex";
+  } else {
+    localStorage.setItem("isBookMarkBarOpen", "open");
+    bookmarkBar.style.display = "block";
+    bookmarkOpen.style.display = "flex";
+    bookmarkClose.style.display = "none";
+  }
+
+  const bookmarkBarToggle = () => {
+    const isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
+    if (isBookMarkBarOpen === "close") {
+      localStorage.setItem("isBookMarkBarOpen", "open");
+      bookmarkBar.style.display = "block";
+      bookmarkOpen.style.display = "flex";
+      bookmarkClose.style.display = "none";
+      return;
+    }
+    localStorage.setItem("isBookMarkBarOpen", "close");
+    bookmarkBar.style.display = "none";
+    bookmarkOpen.style.display = "none";
+    bookmarkClose.style.display = "flex";
+  };
+
+  document
+    .getElementById("bookmark-open-btn")
+    .addEventListener("click", bookmarkBarToggle);
+  document
+    .getElementById("bookmark-close-btn")
+    .addEventListener("click", bookmarkBarToggle);
+})();
